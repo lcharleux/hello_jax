921c70a27125f0739e64a59ef413f112615259b1
Ludovic Charleux <ludovic.charleux@univ-smb.fr>
2025-12-02 15:02:21 +0100

    Update documentation

--- build/_shared/brainfuck-YU7EB23L.js
+++ build/_shared/brainfuck-YU7EB23L.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-UUIXVG6L.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"hello_jax//build/_shared/chunk-UUIXVG6L.js";import"hello_jax//build/_shared/chunk-OZE3FFNP.js";export default a();
