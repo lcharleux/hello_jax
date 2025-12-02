c6c16ba839cf42eba2247e40efce55d6bc48a2fe
Ludovic Charleux <ludovic.charleux@univ-smb.fr>
2025-12-02 14:16:11 +0100

    Update documentation

--- build/_shared/unified-DL6LFCCS.js
+++ build/_shared/unified-DL6LFCCS.js
@@ -1,1 +1,1 @@
-import{b as a}from"/hello_jax/build/_shared/chunk-7SAO57PG.js";import"/hello_jax/build/_shared/chunk-O4VQNZ62.js";import"/hello_jax/build/_shared/chunk-OZE3FFNP.js";export{a as unified};
+import{b as a}from"hello_jax//build/_shared/chunk-7SAO57PG.js";import"hello_jax//build/_shared/chunk-O4VQNZ62.js";import"hello_jax//build/_shared/chunk-OZE3FFNP.js";export{a as unified};
